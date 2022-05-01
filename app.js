6815943037bc74c8acce26b60258081539d405a7
Woony <clover7kso@naver.com>
2022-05-01 14:33:53 +0900

    feat: add checking exist nodes

--- app.js
+++ app.js
@@ -1,92 +1,96 @@
 const { gql, ApolloServer } = require("apollo-server");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
+const { OGM } = require("@neo4j/graphql-ogm");
 const neo4j = require("neo4j-driver");
 
 const AURA_ENDPOINT = "neo4j+s://1eed4b64.databases.neo4j.io";
 const USERNAME = "neo4j";
 const PASSWORD = "1LlRRhK6aoBn-ofn12xZXJ0-hWRAoGamSPR2bB1ykbg";
 
-const resolvers = {
-  Query: {
-    IS_EXIST: (_, { E1_value, E1_type, E2_value, E2_type, REL }, ___) => {
-      console.log(E1_value);
-      console.log(E1_type);
-      console.log(E2_value);
-      console.log(E2_type);
-      console.log(REL);
-      return true;
-    },
-  },
-};
-
-const typeDefs = gql`
-  type Query {
-    IS_EXIST(
-      E1_value: String!
-      E1_type: String!
-      E2_value: String!
-      E2_type: String!
-      REL: String!
-    ): Boolean!
-  }
-  type Query {
-    countRelation: Int
-      @cypher(
-          statement: """
-          MATCH (n)-[r]->(x)
-          RETURN count(r)
-          """
-      )
-  }
-  type Query {
-    countNode: Int
-      @cypher(
-        statement: """
-        MATCH (n)
-        RETURN count(*)
-        """
-      )
-  }
-  type Mutation {
-    deleteNode: Boolean
-      @cypher(
-        statement: """
-          MATCH (n)
-          WITH n
-          ORDER BY n.createdAt
-          LIMIT 1
-          DETACH DELETE n
-          RETURN n
-          """
-      )
-  }
-  type MY_NODE {
-    value: String!
-    type: String!
-    relation: [MY_NODE!]!
-      @relationship(type: "REL", properties: "REL", direction: IN)
-    createdAt: DateTime! @timestamp(operations: [CREATE])
-    updatedAt: DateTime! @timestamp(operations: [UPDATE])
-  }
-  interface REL @relationshipProperties {
-    name: [String!]
-  }
-`;
-
 const driver = neo4j.driver(
   AURA_ENDPOINT,
   neo4j.auth.basic(USERNAME, PASSWORD)
 );
 
+const typeDefs = `
+    type MyNode {
+      type: String!
+      value: String!
+      rel: [MyNode!]! @relationship(type: "REL", direction: OUT)
+    }
+
+    type Mutation {
+      insertTwoNodes(
+        E1_value: String!
+        E1_type: String!
+        E2_value: String!
+        E2_type: String!
+        REL: String!
+      ): Boolean!
+    }
+`;
+
+const ogm = new OGM({ typeDefs, driver });
+const MyNode = ogm.model("MyNode");
+
+const resolvers = {
+  Mutation: {
+    insertTwoNodes: async (
+      _,
+      { E1_value, E1_type, E2_value, E2_type, REL },
+      ___
+    ) => {
+      //Node 존재여부 체크필요
+      let isNode1Exist = await MyNode.find({
+        where: { value: E1_value, type: E1_type },
+      });
+      let isNode2Exist = await MyNode.find({
+        where: { value: E2_value, type: E2_type },
+      });
+      isNode1Exist = isNode1Exist.length >= 1;
+      isNode2Exist = isNode2Exist.length >= 1;
+
+      //둘다 존재하지 않을때
+      if (!isNode1Exist && !isNode2Exist) {
+        MyNode.create({
+          input: [
+            {
+              value: E1_value,
+              type: E1_type,
+              rel: {
+                create: {
+                  node: { value: E2_value, type: E2_type },
+                },
+              },
+            },
+          ],
+        });
+        My;
+      }
+      //node2만 존재할때
+      else if (!isNode1Exist && isNode2Exist) {
+      }
+      //node1만 존재할때
+      else if (isNode1Exist && !isNode2Exist) {
+      }
+      //둘다 존재할때
+      else {
+      }
+      return true;
+    },
+  },
+};
+
 const neo4jSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
 
 // Generate schema
-neo4jSchema.getSchema().then((schema) => {
+Promise.all([neo4jSchema.getSchema(), ogm.init()]).then(([schema]) => {
   const server = new ApolloServer({
     schema,
+    context: ({ req }) => ({ req }),
   });
 
-  server.listen({ port: 4000 }).then(({ url }) => {
-    console.log(`GraphQL server ready on ${url}`);
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
   });
 });
