1d3b741b39a2119dca55b8eb50d52787cf63a2eb
rang21c <rang21c@naver.com>
2022-05-01 16:34:32 +0900

    feat: counting edit

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const AURA_ENDPOINT = "neo4j+s://1eed4b64.databases.neo4j.io";
 const USERNAME = "neo4j";
 const PASSWORD = "1LlRRhK6aoBn-ofn12xZXJ0-hWRAoGamSPR2bB1ykbg";
 
+const NODE_LIMIT = 49998
+const RELATION_LIMIT = 174999
+
 const driver = neo4j.driver(
   AURA_ENDPOINT,
   neo4j.auth.basic(USERNAME, PASSWORD)
@@ -16,13 +19,26 @@ const typeDefs = `
     type MyNode {
       type: String!
       value: String!
-      rel: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: OUT)
+      relOut: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: OUT)
+      relIn: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: IN)
     }
-    
     interface MyRel @relationshipProperties {
       name: [String!]
     }
-
+    type Description {
+      countNode: Int @cypher(
+        statement: """
+        MATCH(N)
+        RETURN COUNT(N)
+        """
+      ),
+      countRelation: Int @cypher(
+        statement: """
+        MATCH(N)-[R]->(X)
+        RETURN COUNT(R)
+        """
+      )
+    }
     type Mutation {
       insertTwoNodes(
         E1_value: String!
@@ -36,14 +52,25 @@ const typeDefs = `
 
 const ogm = new OGM({ typeDefs, driver });
 const MyNode = ogm.model("MyNode");
+const Description = ogm.model("Description");
 
 const resolvers = {
   Mutation: {
     insertTwoNodes: async (
-      _,
+      req,
       { E1_value, E1_type, E2_value, E2_type, REL },
       ___
     ) => {
+      console.log(req)
+      let nodeNum = await (await MyNode.find()).length
+      let relationNum = await MyNode.find()
+      if (nodeNum>=NODE_LIMIT) {
+        //노드 2개 삭제 후 관련 RELATION 모두 삭제
+      }
+      if (relationNum>=RELATION_LIMIT) {
+        //1개 RELATION 삭제 후 노드 남겨둠
+      }
+
       //Node 존재여부 체크필요
       let isNode1Exist = await MyNode.find({
         where: { value: E1_value, type: E1_type },
@@ -53,7 +80,7 @@ const resolvers = {
       });
       isNode1Exist = isNode1Exist.length >= 1;
       isNode2Exist = isNode2Exist.length >= 1;
-
+/*
       //둘다 존재하지 않을때
       if (!isNode1Exist && !isNode2Exist) {
         MyNode.create({
@@ -61,7 +88,7 @@ const resolvers = {
             {
               value: E1_value,
               type: E1_type,
-              rel: {
+              relOut: {
                 create: {
                   node: { value: E2_value, type: E2_type },
                   edge: {
@@ -75,13 +102,67 @@ const resolvers = {
       }
       //node2만 존재할때
       else if (!isNode1Exist && isNode2Exist) {
+        MyNode.create({
+          input: [
+            {
+              value: E1_value,
+              type: E1_type,
+              relOut: {
+                connect: {
+                  where:{
+                    node: { value: E2_value, type: E2_type },
+                  },
+                  edge: {
+                    name: REL,
+                  },
+                },
+              },
+            },
+          ],
+        });
       }
       //node1만 존재할때
       else if (isNode1Exist && !isNode2Exist) {
+        MyNode.create({
+          input: [
+            {
+              value: E2_value,
+              type: E2_type,
+              relIn: {
+                connect: {
+                  where:{
+                    node: { value: E1_value, type: E1_type },
+                  },
+                  edge: {
+                    name: REL,
+                  },
+                },
+              },
+            },
+          ],
+        });
       }
       //둘다 존재할때
       else {
-      }
+        MyNode.update({
+          where: { 
+            value: E1_value,
+            type: E1_type
+          },
+          update: { 
+            relOut: {
+              connect: {
+                where:{
+                  node: { value: E2_value, type: E2_type },
+                },
+                edge: {
+                  name: REL,
+                },
+              },
+            },
+          },
+        })
+      }*/
       return true;
     },
   },
