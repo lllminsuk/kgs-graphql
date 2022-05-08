c611b2d9e86371ac84d00c363bdef946375d60f2
rang21c <rang21c@naver.com>
2022-05-08 21:47:04 +0900

    feat: complete insertTwoNodes

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const AURA_ENDPOINT = "neo4j+s://1eed4b64.databases.neo4j.io";
 const USERNAME = "neo4j";
 const PASSWORD = "1LlRRhK6aoBn-ofn12xZXJ0-hWRAoGamSPR2bB1ykbg";
 
-const NODE_LIMIT = 5//49998
-const RELATION_LIMIT = 5//174999
+const NODE_LIMIT = 100//50000
+const RELATION_LIMIT = 5//175000
 
 const driver = neo4j.driver(
   AURA_ENDPOINT,
@@ -19,11 +19,10 @@ const typeDefs = `
     type MyNode {
       type: String!
       value: String!
-      createdAt: DateTime! @timestamp(operations: [CREATE])
+      createdAt: DateTime! @timestamp(operations: [CREATE, UPDATE])
       relOut: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: OUT)
       relIn: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: IN)
     }
-    
     interface MyRel @relationshipProperties {
       name: [String!]
     }
@@ -42,10 +41,39 @@ const typeDefs = `
         REL: String!
       ): Boolean!
     }
+    type Info {
+      id: Int!
+      countNode: Int!
+      @cypher(
+        statement: """
+        MATCH (n)
+        RETURN count(*)
+        """
+      )
+      countRelation: Int!
+      @cypher(
+            statement: """
+            MATCH (n)-[r]->(x)
+            RETURN count(r)
+            """
+        )
+      deleteTwoNodes: Boolean
+      @cypher(
+            statement: """
+            MATCH (N) 
+            WHERE N.value IS NOT NULL 
+            WITH N 
+            ORDER BY N.createdAt 
+            LIMIT 2 
+            DETACH DELETE N
+            """
+      )
+    }
 `;
 
 const ogm = new OGM({ typeDefs, driver });
 const MyNode = ogm.model("MyNode");
+const Info = ogm.model("Info")
 
 const resolvers = {
   Query: {
@@ -130,37 +158,18 @@ const resolvers = {
     ) => {
       const selectionSet = `
           {
-              relOut {
-                type
-              }
+            countNode
+            countRelation
           }
       `;
-      let nodeNum = await (await MyNode.find()).length //노드 개수
-      let relationNum = await MyNode.find({ selectionSet }).then((nodes)=>{ //관계 개수
-        let num = 0
-        nodes.map((items)=>{
-          num += items.relOut.length
-        })
-        return num
+      let desc
+      await Info.find({ selectionSet:selectionSet }).then((nodes)=>{ //관계 개수
+        desc = nodes[0];
       })
-      console.log(relationNum)
-
-      if (nodeNum>=NODE_LIMIT) {
-        //노드 2개 삭제 후 관련 RELATION 모두 삭제
-        await MyNode.delete({
-          where: {
-            
-          }
-        });
-      }
-      if (relationNum>=RELATION_LIMIT) {
-        //1개 RELATION 삭제 후 노드 남겨둠
-        await MyNode.delete({
-          where: {
-
-          }
-        });
-      }
+      
+      let nodeNum = desc.countNode //노드 개수
+      let relationNum = desc.countRelation
+      console.log(nodeNum, relationNum)
 
       //Node 존재여부 체크필요
       let isNode1Exist = await MyNode.find({
@@ -172,6 +181,122 @@ const resolvers = {
       isNode1Exist = isNode1Exist.length >= 1;
       isNode2Exist = isNode2Exist.length >= 1;
 
+      if (!isNode1Exist && !isNode2Exist && nodeNum+2 > NODE_LIMIT) {
+        console.log("deleteTwoNode")
+        //노드 2개 삭제 후 관련 RELATION 모두 삭제
+        let findDeleteNode = await MyNode.find({
+          options: {
+            sort: [
+              {
+                createdAt: "ASC"
+              }
+            ],
+            limit: 2
+          }
+        })
+        findDeleteNode.map(async(node) => {
+          await MyNode.delete({
+            where: {
+              type: node.type,
+              value: node.value,
+            },
+          })
+        })
+      }
+      else if (((!isNode1Exist&&isNode2Exist) || (isNode1Exist&&!isNode2Exist)) && nodeNum+1 > NODE_LIMIT) {
+        console.log("deleteOneNode")
+        //노드 2개 삭제 후 관련 RELATION 모두 삭제
+        let findDeleteNode = await MyNode.find({
+          options: {
+            sort: [
+              {
+                createdAt: "ASC"
+              }
+            ],
+            limit: 1
+          }
+        })
+        findDeleteNode.map(async(node) => {
+          await MyNode.delete({
+            where: {
+              type: node.type,
+              value: node.value,
+            },
+          })
+        })
+      }
+      
+        let relExist = await MyNode.find({
+          where: {
+            value: E1_value,
+            type: E1_type,
+            relOutConnection_SINGLE: {
+              node: {
+                value: E2_value,
+                type: E2_type
+              },
+            }
+          }
+        })
+        relExist = relExist.length > 0
+
+      if (!(isNode1Exist && isNode2Exist && relExist) && relationNum+1 > RELATION_LIMIT) {
+        const selectionSet2 = `
+          {
+            type
+            value
+            relOut {
+              type
+              value
+            }
+          }
+        `;
+
+        let a = await MyNode.find({ selectionSet:selectionSet2, 
+          where: {
+            relOutAggregate: {
+              count_GT: 0
+            }
+          },
+          options: {
+            sort: [
+              {
+                createdAt: "ASC"
+              }
+            ],
+            limit: 1
+          }
+        })
+        let nodeType1 = a[0].type
+        let nodeValue1 = a[0].value
+        let nodeType2 = a[0].relOut[a[0].relOut.length-1].type
+        let nodeValue2 = a[0].relOut[a[0].relOut.length-1].value 
+        console.log(nodeType1, nodeValue1, nodeType2, nodeValue2)
+        await MyNode.update({
+          where: {
+            type: nodeType1,
+            value: nodeValue1
+          },
+          update: {
+            relOut: [
+              {
+                disconnect: [
+                  {
+                    where: {
+                      node: {
+                        type: nodeType2,
+                        value: nodeValue2
+                      }
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        })
+        console.log(relExist)
+      }
+      
       //둘다 존재하지 않을때
       if (!isNode1Exist && !isNode2Exist) {
         MyNode.create({
@@ -265,10 +390,24 @@ const neo4jSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
 Promise.all([neo4jSchema.getSchema(), ogm.init()]).then(([schema]) => {
   const server = new ApolloServer({
     schema,
-    context: ({ req }) => ({ req }),
+    context: async({ req }) => {
+      return { req }
+    },
   });
-
-  server.listen().then(({ url }) => {
+  
+  server.listen().then(async({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
+    let isInfoExist = await Info.find({
+      where: { id: 0 },
+    });
+    if(isInfoExist.length===0){
+      await Info.create({
+        input: [
+          {
+            id: 0
+          }
+        ]
+      })
+    }
   });
 });
