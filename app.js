55436f6810a81204a7763bcb8355f6a836296b90
rang21c <rang21c@naver.com>
2022-05-08 13:20:57 +0900

    feat: add select

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const AURA_ENDPOINT = "neo4j+s://1eed4b64.databases.neo4j.io";
 const USERNAME = "neo4j";
 const PASSWORD = "1LlRRhK6aoBn-ofn12xZXJ0-hWRAoGamSPR2bB1ykbg";
 
-const NODE_LIMIT = 49998
-const RELATION_LIMIT = 174999
+const NODE_LIMIT = 5//49998
+const RELATION_LIMIT = 5//174999
 
 const driver = neo4j.driver(
   AURA_ENDPOINT,
@@ -19,6 +19,7 @@ const typeDefs = `
     type MyNode {
       type: String!
       value: String!
+      createdAt: DateTime! @timestamp(operations: [CREATE])
       relOut: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: OUT)
       relIn: [MyNode!]! @relationship(type: "REL", properties: "MyRel", direction: IN)
     }
@@ -26,7 +27,12 @@ const typeDefs = `
     interface MyRel @relationshipProperties {
       name: [String!]
     }
-
+    type Query {
+      findByValue(
+        E1_value: String!
+        E2_value: String!
+      ): MyNode!
+    }
     type Mutation {
       insertTwoNodes(
         E1_value: String!
@@ -42,6 +48,80 @@ const ogm = new OGM({ typeDefs, driver });
 const MyNode = ogm.model("MyNode");
 
 const resolvers = {
+  Query: {
+    findByValue: async (
+      _,
+      { E1_value, E2_value },
+      ___
+    ) => {
+      const selectionSet = `
+        {
+          type
+          value
+          relOut {
+            type
+            value
+          }
+          relIn {
+            type
+            value
+          }
+        }
+      `;
+      let isNode1Exist = await MyNode.find({
+        where: { value: E1_value },
+      });
+      let isNode2Exist = await MyNode.find({
+        where: { value: E2_value },
+      });
+      isNode1Exist = isNode1Exist.length >= 1;
+      isNode2Exist = isNode2Exist.length >= 1;
+      //둘다 존재하지 않을때
+      if (!isNode1Exist && !isNode2Exist) {
+        return false
+      }
+      //node2만 존재할때
+      else if (!isNode1Exist && isNode2Exist) {
+        return MyNode.find({ selectionSet }).then((node) => {
+          return MyNode.find({ selectionSet }).then((node) => {
+            for (var i=0; i<node.length; i++) {
+              if (node[i].value===E2_value) {
+                console.log(node[i])
+                return node[i]
+              }
+            }
+          })
+        })
+      }
+      //node1만 존재할때
+      else if (isNode1Exist && !isNode2Exist) {
+        return MyNode.find({ selectionSet }).then((node) => {
+          for (var i=0; i<node.length; i++) {
+            if (node[i].value===E1_value) {
+              console.log(node[i])
+              return node[i]
+            }
+          }
+        })
+      }
+      //둘다 존재할때
+      else {
+        var nodeArr = {}
+        MyNode.find({ selectionSet }).then((node) => {
+          for (var i=0; i<node.length; i++) {
+            if (node[i].value===E1_value) {
+              
+            }
+            if (node[i].value===E2_value) {
+              
+            }
+          }
+        })
+        console.log(nodeArr)
+        return nodeArr
+      }
+    },
+  },
   Mutation: {
     insertTwoNodes: async (
       _,
@@ -55,8 +135,8 @@ const resolvers = {
               }
           }
       `;
-      let nodeNum = await (await MyNode.find()).length
-      let relationNum = await MyNode.find({ selectionSet }).then((nodes)=>{
+      let nodeNum = await (await MyNode.find()).length //노드 개수
+      let relationNum = await MyNode.find({ selectionSet }).then((nodes)=>{ //관계 개수
         let num = 0
         nodes.map((items)=>{
           num += items.relOut.length
@@ -67,9 +147,19 @@ const resolvers = {
 
       if (nodeNum>=NODE_LIMIT) {
         //노드 2개 삭제 후 관련 RELATION 모두 삭제
+        await MyNode.delete({
+          where: {
+            
+          }
+        });
       }
       if (relationNum>=RELATION_LIMIT) {
         //1개 RELATION 삭제 후 노드 남겨둠
+        await MyNode.delete({
+          where: {
+
+          }
+        });
       }
 
       //Node 존재여부 체크필요
