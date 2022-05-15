799eaedb05686f8562c2afb6c9ab1cfcf8557d68
rang21c <rang21c@naver.com>
2022-05-15 15:56:06 +0900

    relation label mapping to Korean

--- app.js
+++ app.js
@@ -91,6 +91,37 @@ const typeDefs = `
 const ogm = new OGM({ typeDefs, driver });
 const MyNode = ogm.model("MyNode");
 const Info = ogm.model("Info");
+const relTag = {
+  'no_relation':"관계 없음",
+  'org:dissolved':'해당 기관이 해체된 날짜',
+  'org:founded':'해당 기관이 설립된 날짜',
+  'org:place_of_headquarters':'해당 기관의 본사가 위치한 장소',
+  'org:alternate_names':'해당 기관의 대체 이름',
+  'org:member_of':'해당 기관의 일원',
+  'org:members':'해당 기관에 속한 하위기관',
+  'org:political/religious_affiliation':'해당 기관이 소속된 정치/종교 그룹',
+  'org:product':'해당 기관에서 생산된 제품',
+  'org:founded_by':'해당 기관의 설립자',
+  'org:top_members/employees':'해당 기관의 대표 구성원',
+  'org:number_of_employees/members':'해당 기관의 소속된 구성원 수',
+  'per:date_of_birth':'해당 사람이 태어난 날짜',
+  'per:date_of_death':'해당 사람이 죽은 날짜',
+  'per:place_of_birth':'해당 사람이 태어난 장소',
+  'per:place_of_death':'해당 사람이 죽은 장소',
+  'per:place_of_residence':'해당 사람이 거주하는 장소',
+  'per:origin':'해당 사람의 국적',
+  'per:employee_of':'해당 사람이 근무하는 기관',
+  'per:schools_attended':'해당 사람의 출신 학교',
+  'per:alternate_names':'해당 사람의 대체 이름',
+  'per:parents':'해당 사람의 부모님',
+  'per:siblings':'해당 사람의 자식',
+  'per:spouse':'해당 사람의 배우자',
+  'per:other_family':'해당 사람의 부모, 배우자, 자식을 제외한 나머지 가족',
+  'per:colleagues':'해당 사람의 직장동료',
+  'per:product':'해당 사람이 생산한 제품',
+  'per:religion':'해당 사람의 종교',
+  'per:title':'해당 사람의 직위'
+}
 
 const resolvers = {
   Query: {
@@ -179,7 +210,6 @@ const resolvers = {
       links = links.filter((link, idx, arr)=>{
         return arr.findIndex((item) => item.source === link.source && item.target === link.target && item.label === link.label) === idx
       });
-      console.log(nodes, links)
       for (var i=limit; i<nodes.length; i++) {
         for (var j=0; j<links.length; j++) {
           if (links[j].source===nodes[i].id || links[j].target===nodes[i].id) {
@@ -205,6 +235,9 @@ const resolvers = {
           links.splice(i,1)
           i--
         }
+      }
+      for (var i=0; i<links.length; i++) {
+        links[i].label = relTag[links[i].label]
       }
       console.log({ nodes, links });
 
